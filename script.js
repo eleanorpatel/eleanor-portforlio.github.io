dcb52efbf987b2d415faf29d120ac22f93663505
eleanorpatel <eleanor.v.patel@gmail.com>
2026-05-18 18:02:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const desctext = document.getElementById("desctext")
 const changeimage = document.getElementById("changeimage")
 const next = document.getElementById("next")
 
+let projectNames = ["Peacock", "Geoguesser", "Blank Space"] 
+
 let displayState = 0;
 
 function changeProject(){
   let cs = (displayState % 2).toString()
+  name.textContent = projectNames[cs]
   desctext.textContent = `This is the description for project${cs}`
   changeimage.src = `project${cs}.png`
   displayState += 1
 };
 
 next.addEventListener("click", changeProject);
+
